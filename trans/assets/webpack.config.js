4209e40686afc4550a324a0aa42009c518b4dd62
jakemittlemanwebdev2019 <megamanzerox@gmail.com>
2019-11-17 19:15:33 -0500

    Fixed some bugs. Need to install some packages

--- trans/assets/webpack.config.js
+++ trans/assets/webpack.config.js
@@ -1,4 +1,3 @@
-  
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -27,7 +26,10 @@ module.exports = (env, options) => ({
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
         }
       },
       {
@@ -39,6 +41,9 @@ module.exports = (env, options) => ({
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       },
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
